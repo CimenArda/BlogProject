881efe571f36e2271a31e8d0ca74370d83d52009
ArdaCimen <cimenarda.yaz@gmail.com>
2024-12-05 00:57:56 +0300

    Contact Sayfası İşlemleri Sweet Alert Eklenmesi

---
+++ BlogProject.PresentationLayer/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SensiveBlogProject.BusinessLayer.Abstract;
+using SensiveBlogProject.EntityLayer.Concrete;
+
+namespace BlogProject.PresentationLayer.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            try
+            {
+                contact.Created = DateTime.Now;
+                _contactService.TInsert(contact);
+                return Json(new { success = true, message = "Mesajını Aldık!" });
+            }
+            catch (Exception)
+            {
+                return Json(new { success = false, message = "Mesaj Gönderme işlemi sırasında bir hata oluştu." });
+            }
+          
+
+            return View();
+        }
+    }
+}
