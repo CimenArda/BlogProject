96dca0fe02cb116c6c97935b51194212b36b01d9
ArdaCimen <cimenarda.yaz@gmail.com>
2024-12-05 12:10:15 +0300

    Admin(Author) Panelinin Componentlere Ayrılması

---
+++ BlogProject.PresentationLayer/Areas/Author/Controllers/AuthorCategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.PresentationLayer.Areas.Author.Controllers
+{
+    [Area("Author")]
+    public class AuthorCategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v3 = "Kategori İşlemleri";
+            return View();
+        }
+    }
+}
